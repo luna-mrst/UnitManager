3d66677162048f9598123f9601dd4eb6b3f4e7bc
luna-mrst <crf107@i.softbank.jp>
2019-01-30 00:10:57 +0900

    期待値でレアゴル費用を計算

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -12,7 +12,7 @@ export default class Info extends React.Component<InfoProps> {
         <p>覚醒書総数： ★3={this.props.book3} ★4={this.props.book4} ★5={this.props.book5}</p>
         <p>必要メダル： {this.props.medalCount}枚 (15覚醒{this.props.evolCount}人分)</p>
         <p>素材総数： 蕾={this.props.budCount}個 花={this.props.flowerCount}個</p>
-        <p>レアゴル費用： 約{this.props.scoutCost}ゴルド <span style={{ color: 'red' }}>メンテ中だよ...！</span></p>
+        <p>レアゴル費用： 約{this.props.scoutCost}ゴルド</p>
         <p>レベル上げ費用： {this.props.levelUpCost}ゴルド</p>
       </div>
     );
