41710ca2f153e46d8c543e204407b09ac2862046
luna-mrst <crf107@i.softbank.jp>
2019-01-29 14:13:47 +0900

    足りなかった設定を追加

--- development.js
+++ development.js
@@ -5,41 +5,44 @@ const src = path.resolve(__dirname, 'src');
 const dist = path.resolve(__dirname, 'dist');
 
 export default {
-    mode: 'development',
-    target: 'web',
-    node: { fs: 'empty' },
-    entry: src + '/main.tsx',
+  mode: 'development',
+  target: 'web',
+  node: { fs: 'empty' },
+  entry: src + '/main.tsx',
 
-    output: {
-        path: dist,
-        filename: 'bundle.js'
-    },
+  output: {
+    path: dist,
+    filename: 'bundle.js'
+  },
 
-    devtool: "source-map",
+  devtool: "source-map",
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            }
-        ]
-    },
-
-    resolve: {
-        extensions: ['.js', 'ts', '.tsx']
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'index.html'),
-            filename: 'index.html'
-        })
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'ts-loader' }
+        ],
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
     ]
+  },
+
+  resolve: {
+    extensions: ['.js', 'ts', '.tsx']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      filename: 'index.html'
+    })
+  ]
 }
