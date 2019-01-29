74ededb5866c46361e3a05b040ffcb1ce9de0542
luna-mrst <crf107@i.softbank.jp>
2019-01-29 09:27:19 +0900

    TSでReact使う準備

--- development.js
+++ development.js
@@ -8,25 +8,32 @@ export default {
     mode: 'development',
     target: 'web',
     node: { fs: 'empty' },
-    entry: src + '/main.jsx',
+    entry: src + '/main.tsx',
 
     output: {
         path: dist,
         filename: 'bundle.js'
     },
 
+    devtool: "source-map",
+
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.tsx$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
             }
         ]
     },
 
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', 'ts', '.tsx']
     },
 
     plugins: [
